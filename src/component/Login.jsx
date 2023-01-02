3415f24f09178c436e8674793320dba5afc66399
Gabbee24 <atunwagabriel@gmail.com>
2023-01-02 23:20:45 +0100

    learnt about useDispatch and useSelector

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { login, logout } from '../features/User';
 
 const Login = () => {
+    const dispatch = useDispatch();
 
   return (
     <div>
-        <button>
+        <button 
+            onClick={
+                () => {dispatch(login({
+                    name: 'Gabby', 
+                    age: 22, 
+                    email:'atunwagabriel@gmail.com'
+                    }))}}>
             Login
+        </button>
+        
+        <button onClick={() => {
+            dispatch(logout());
+        }}>
+            LOGOUT
         </button>
     </div>
   )
